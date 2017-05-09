4de1851db23d2f74a76fc2f182e09b80cb271b74
Bernardo <bsabatini@hms.harvarde.edu>
2017-05-09 09:19:07 -0400

    Initial upload
    First set of routines

---
+++ BS usefulRoutines.R
@@ -0,0 +1,59 @@
+#Useful routines from Bernardo
+
+# pass in a marker object produced by FindAllMarkers and return the top N genes in cluster=clustNum
+topNGenes <- function(markers, clustNum, N=20) {
+  head(subset(markers, cluster==clustNum), N)
+}
+
+# returns the genes with avg_diff>thresh (or avg_diff<thresh if thresh<0) in cluster=clustNum
+# markers is a FindAllMarkers object
+# N limits number of genes to be return but defaults to NULL which means return all genes
+topTGenes <- function(markers, clustNum, thresh=0.1, N=NULL) {
+  if (thresh>=0) {
+    if (is.null(N))
+      subset(markers, (cluster==clustNum)&(avg_diff>thresh))
+    else
+      head(subset(markers, (cluster==clustNum)&(avg_diff>thresh)), N)
+  } else {
+    if (is.null(N))
+      subset(markers, (cluster==clustNum)&(avg_diff<thresh))
+    else
+      head(subset(markers, (cluster==clustNum)&(avg_diff<thresh)), N)
+  }
+}
+
+# cycleAllTop - 
+#   sObj : a Seurat object
+#   markers : results of FindAllMarkers
+#   clusterIds : a list of the cluster numbers to cycle through
+#   nGenes : how many genes for each cluster to display
+#   allAtOnce : if F, one FeaturePlot per gene.  if T, put them all in one panel.  T only suitable for upto 4 genes or so
+#   
+#   Cycles through the top nGenes in each of the specified clusters and does the FeaturePlot for each.
+#   recommended that you first open a pdf output file (pdf('filename.pdf)), run cycleAllTop, and then close pdf (dev.off())
+cycleAllTop <- function(sObj, markers, clusterIds, nGenes, allAtOnce=F) {
+  for (counter in clusterIds) {
+    print(paste("Cluster", counter))
+    tt=topTGenes(markers, counter, 0, nGenes)
+    cGenes=tt$gene
+    print(paste("Top", nGenes, "enriched genes"))
+    print(cGenes)
+    
+    textplot(paste("Cluster", counter, "Top", nGenes, "enriched genes"),cex=2)
+    textplot(cGenes,cex=2)
+    
+    if (allAtOnce) FeaturePlot(sObj, cGenes)  
+    else for (gene in cGenes) FeaturePlot(sObj, gene)
+  }
+}
+
+
+# randomCells - return the names of a random subsets of the cells an Seurat object
+randomCells <- function(sObj, fract=0.1) {
+  nCells=dim(sObj@data)[2]
+  colnames(sObj@data)[sample(1:nCells, floor(nCells*fract), replace = F)]
+}
+
+# smallTSNEPlot - calls TSNEPlot for randome subset of cells
+smallTSNEPlot <- function(sObj, fract=0.1, labelsOn=T) TSNEPlot(sObj, cells.use = randomCells(sObj, fract), do.label=labelsOn)
+
